9456fa28f5f6a401f5e7074b9abf9853d11681c4
Alessandro Viganò <alvistar@gmail.com>
2020-05-05 15:28:46 +0200

    Fixed pipe for sed

--- poetry-export-sed.sh
+++ poetry-export-sed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-poetry export $@ -f requirements.txt -o requirements.txt | sed 's/-e //' >requirements.txt
+poetry export $@ -f requirements.txt | sed 's/-e //' >requirements.txt
 git diff --exit-code requirements.txt 
 exit
